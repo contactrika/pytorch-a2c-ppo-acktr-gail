691515d440dd454eec3a466bc0acfd829a58b99c
contactrika <contact.rika@gmail.com>
2019-04-23 22:58:39 +0200

    pivoting

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -233,19 +233,22 @@ class MLPBase(NNBase):
 class MLPBaseLong(NNBase):
     def __init__(self, num_inputs, recurrent=False, hidden_size=64):
         super(MLPBaseLong, self).__init__(recurrent, num_inputs, hidden_size)
-        nl = nn.ReLU()
+        nl = nn.Tanh()
 
         if recurrent:
             num_inputs = hidden_size
 
         self.shared = nn.Sequential(
             nn.Linear(num_inputs, hidden_size), nl,  # nn.Dropout(p=0.2),
+            nn.Linear(hidden_size, hidden_size), nl,
             nn.Linear(hidden_size, hidden_size), nl)
 
         self.actor = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size), nl,
             nn.Linear(hidden_size, hidden_size), nn.Tanh())
 
         self.critic = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size), nl,
             nn.Linear(hidden_size, hidden_size), nl)
 
         self.critic_linear = nn.Linear(hidden_size, 1)

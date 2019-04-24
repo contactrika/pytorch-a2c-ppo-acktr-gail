d83d5e6a3cfe71c36e916ee1f43ed4190213366b
contactrika <contact.rika@gmail.com>
2019-04-24 15:16:08 +0200

    pivoting

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -266,6 +266,10 @@ class MLPBaseLong(NNBase):
         self.critic = nn.Sequential(
             nn.Linear(num_inputs, hidden_size), nl,
             nn.Linear(hidden_size, hidden_size), nl,
+            nn.Linear(hidden_size, 1))
+
+        self.twin_critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size), nl,
             nn.Linear(hidden_size, hidden_size), nl,
             nn.Linear(hidden_size, 1))
 
@@ -276,6 +280,7 @@ class MLPBaseLong(NNBase):
         self.actor.apply(init_weights)
         self.twin_actor.apply(init_weights)
         self.critic.apply(init_weights)
+        self.twin_critic.apply(init_weights)
 
         self.train()
 
@@ -285,11 +290,10 @@ class MLPBaseLong(NNBase):
         if self.is_recurrent:
             x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
 
-        act_part = self.actor(x)
-        twin_act_part = self.twin_actor(x)
         # TODO: check that this passes only grads of net parts actually used.
         # TODO: generalize later.
         phi_tl_minus_tgt = (x[:,0]-x[:,5]).view(-1, 1)
-        act_mean = torch.where(phi_tl_minus_tgt>0, twin_act_part, act_part)
+        act_mean = torch.where(phi_tl_minus_tgt>0, self.twin_actor(x), self.actor(x))
+        crit = torch.where(phi_tl_minus_tgt>0, self.twin_critic(x), self.critic(x))
 
-        return self.critic(x), act_mean, rnn_hxs
+        return crit, act_mean, rnn_hxs
